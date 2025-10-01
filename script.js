85b6ef8bd8e40d19c2e459f431a269eeaacdd21d
devilts <tobysaltmarsh@hotmail.com>
2025-10-01 14:58:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// On settings change
+document.body.classList.toggle('matrix-on', true);
+localStorage.setItem('matrixEnabled', 'true');
+
+// On page load
+if (localStorage.getItem('matrixEnabled') === 'true') {
+  document.body.classList.add('matrix-on');
+  startMatrix(document.getElementById('matrixCanvas'));
+}
 
